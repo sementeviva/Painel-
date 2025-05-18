97143a2c5137fc244fbbe6a32b3cefa76c7dda2d
sementeviva <vogelthiago55@gmail.com>
2025-05-18 20:10:54 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-
+import os
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'sua_chave_super_secreta')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
