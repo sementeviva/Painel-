6b7d5aa3f2964ed3009fd09a38989582d6f3b501
sementeviva <vogelthiago55@gmail.com>
2025-05-18 21:21:42 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = None
+import os
 
 db = SQLAlchemy()
-migrate = None
 
 def create_app():
-    global app, migrate
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app = Flask(__name__, template_folder='templates')
+    app.config['SECRET_KEY'] = 'semente'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
     db.init_app(app)
-    migrate = Migrate(app, db)
 
-    from app import routes
-    app.register_blueprint(routes.bp)
+    from app.routes import bp
+    app.register_blueprint(bp)
 
     return app
